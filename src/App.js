552eae1952f908b2f2514f56b721c08c80fb2591
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-04 03:09:31 -0400

    ocultando botones del navegador y agregar scroll al pasar de pagina

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Formulario from './components/Formulario'
 import ListadoImagenes from './components/ListadoImagenes'
 
@@ -10,37 +10,42 @@ function App() {
   const [imagenes, setImagenes] = useState([]);
   const [paginaActual, setPaginaActual] = useState(1);
   const [totalPaginas, setTotalPaginas] = useState(1);
-  
+
   useEffect(() => {
     const consultarAPI = async () => {
       if (busqueda === '') return;
 
       const imagenesPorPagina = 30;
       const key = '8272475-1276ad683e45d6c4bbb563af4';
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaActual}`;
 
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
 
       setImagenes(resultado.hits);
-      
+
       // calcular el total de paginas
 
       const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
       setTotalPaginas(calcularTotalPaginas);
-    
+
+      // mover la pantalla hacia arriba
+
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior : 'smooth' })
+
     }
 
     consultarAPI();
 
-  }, [busqueda])
+  }, [busqueda,paginaActual])
 
   // definir la pagina anterior
 
   const paginaAnterior = () => {
     const nuevaPaginaActual = paginaActual - 1;
-    
-    if (nuevaPaginaActual === 0) return; 
+
+    if (nuevaPaginaActual === 0) return;
 
     setPaginaActual(nuevaPaginaActual);
   }
@@ -49,8 +54,8 @@ function App() {
 
   const paginaSiguiente = () => {
     const nuevaPaginaActual = paginaActual + 1;
-    
-    if(nuevaPaginaActual > totalPaginas) return;
+
+    if (nuevaPaginaActual > totalPaginas) return;
 
     setPaginaActual(nuevaPaginaActual);
   }
@@ -67,17 +72,22 @@ function App() {
         <ListadoImagenes
           imagenes={imagenes}
         />
-        <button
-          type="button"
-          className="btn btn-info mr-1"
-          onClick={paginaAnterior}
-        >Anterior &laquo;</button>
 
-        <button
-          type="button"
-          className="btn btn-info"
-          onClick={paginaSiguiente}
-        >Siguiente &raquo;</button>
+        {(paginaActual === 1) ? null : (
+          <button
+            type="button"
+            className="btn btn-info mr-1"
+            onClick={paginaAnterior}
+          >Anterior &laquo;</button>
+        )}
+
+        {(paginaActual === totalPaginas) ? null : (
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={paginaSiguiente}
+          >Siguiente &raquo;</button>
+        )}
       </div>
     </div>
   );
